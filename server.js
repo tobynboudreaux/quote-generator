b4e900fb9459f948cc8623bbcf9c6d3bff0157d4
tobynboudreaux <tobynboudreaux@gmail.com>
2020-10-13 13:49:29 -0500

    added server functionality

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = 5000;
 
-axios({
-  method: "GET",
-  url: "https://quotes15.p.rapidapi.com/quotes/random/",
-  headers: {
-    "content-type": "application/octet-stream",
-    "x-rapidapi-host": "quotes15.p.rapidapi.com",
-    "x-rapidapi-key": "77bc85f9femsh8cc7ee10e72cfafp1ccc58jsna4131bc1c32a",
-    useQueryString: true,
-  },
-  params: {
-    language_code: "en",
-  },
-})
-  .then((response) => {
-    display(response.data.originator.name, response.data.content);
+app.get("/", (req, res) => {
+  res.send(quote());
+});
+
+function quote() {
+  axios({
+    method: "GET",
+    url: "https://quotes15.p.rapidapi.com/quotes/random/",
+    headers: {
+      "content-type": "application/octet-stream",
+      "x-rapidapi-host": "quotes15.p.rapidapi.com",
+      "x-rapidapi-key": "77bc85f9femsh8cc7ee10e72cfafp1ccc58jsna4131bc1c32a",
+      useQueryString: true,
+    },
+    params: {
+      language_code: "en",
+    },
   })
-  .catch((error) => {
-    console.log(error);
-  });
+    .then((response) => {
+      display(response.data.originator.name, response.data.content);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 function display(author, content) {
   console.log("Author:", author, "\n", "Content:", content);
